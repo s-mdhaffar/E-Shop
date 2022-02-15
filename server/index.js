2617b54c0cc9b8afbb187ed7176a744357ec6ba3
s-mdhaffar <s.mdhaffar@gmail.com>
2022-02-15 20:21:43 +0100

    register login

--- server/index.js
+++ server/index.js
@@ -3,15 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, () => {
 	console.log('Connected to MongoDB');
 });
 
-app.get('/api/test', () => {
-	console.log('test successful');
-});
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Server connected');
