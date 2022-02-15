a67a31be498d58d30c6230a67cc7b67493caeca0
s-mdhaffar <s.mdhaffar@gmail.com>
2022-02-15 14:21:02 +0100

    setting models

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, () => {
+	console.log('Connected to MongoDB');
+});
+
+app.get('/api/test', () => {
+	console.log('test successful');
+});
+
+app.listen(process.env.PORT || 5000, () => {
 	console.log('Server connected');
 });
