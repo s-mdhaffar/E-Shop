996302a98f6f14f734169a648afadb38d707e869
s-mdhaffar <s.mdhaffar@gmail.com>
2022-02-20 01:27:43 +0100

    responsive newsletter

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import styled from "styled-components"
 import {Search, ShoppingCartOutlined} from "@mui/icons-material"
 import { Badge } from '@mui/material'
+import {mobile} from '../responsive'
 
 const Container = styled.div`
 height:60px;
+${mobile({height:"50px"})}
 `
 
 const Wrapper = styled.div`
@@ -12,6 +14,7 @@ padding:10px 20px;
 display: flex;
 align-items: center;
 justify-content: space-between;
+${mobile({padding:"10px 0px"})}
 `
 
 const Left=styled.div`
@@ -22,6 +25,8 @@ align-items: center;
 
 const Language=styled.div`
     cursor: pointer;
+${mobile({display:"none"})}
+
 `
 
 const SearchContainer=styled.div`
@@ -34,6 +39,8 @@ const SearchContainer=styled.div`
 
 const Input=styled.input`
     border: none;
+${mobile({width:"50px"})}
+
 `
 
 const Center=styled.div`
@@ -43,6 +50,8 @@ text-align: center;
 
 const Logo=styled.h1`
     font-weight: bold;
+${mobile({fontSize:"24px"})}
+
 `
 
 const Right=styled.div`
@@ -50,6 +59,7 @@ flex:1;
 display: flex;
 align-items: center;
 justify-content: flex-end;
+${mobile({flex:2,justifyContent:"center"})}
 `
 
 const MenuItem=styled.div`
@@ -58,6 +68,7 @@ cursor: pointer;
     display: flex;
     align-items: center;
     margin-left: 25px;
+${mobile({fontSize:"12px",margineLeft:"10px"})}
 `
 
 const Navbar = () => {
@@ -67,12 +78,12 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                 <SearchContainer>
-                    <Input/>
+                    <Input placeholder='Search' />
                     <Search style={{color:"gray",fontSize:16}} />
                 </SearchContainer></Left>
                 <Center>
                     <Logo>
-                        Nawara Fashion Store
+                        Nawara
                     </Logo>
                 </Center>
                 <Right>
