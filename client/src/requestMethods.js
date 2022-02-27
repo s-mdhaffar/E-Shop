dd8ce17b355c435e8de9712f3b13a9d0279129b2
s-mdhaffar <s.mdhaffar@gmail.com>
2022-02-27 23:46:02 +0100

    admin

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGQyMTI2NDcyY2JkOTFiYTRjNTgyMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NTUzNzI0MywiZXhwIjoxNjQ1Nzk2NDQzfQ.EEQ5DHqPZSbdJM9GrfR2dZCasr347i4z4zlC-kX2e-o';
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
 	baseURL : BASE_URL
